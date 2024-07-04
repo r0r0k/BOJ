c6494bccb830a632e6f6d3da7e36b573bfc8a835
juhyeongkim527 <kesnt8892@gmail.com>
2024-07-05 05:14:44 +0900

    solved

--- 1436/1436.c
+++ 1436/1436.c
@@ -9,7 +9,7 @@ int main()
     int cmp;       // 어떤 수가 종말 수인지 비교하기 위한 변수
     int six_count; // 연속적인 6의 개수를 저장하는 변수
 
-    while (n != 1) // n == 1일 때는 666이므로
+    while (n != 1) // n == 1일 때는 666이므로 제외하고, n--를해서 n == 1이 될 때 까지 반복
     {
         name++; // brute-force로 계속 name을 증가시키며 만족하는 name 찾기
         six_count = 0;
@@ -18,7 +18,7 @@ int main()
         {
             if (cmp % 10 == 6)
                 six_count++;
-            else // 연속하는 6을 찾기 위해 6이 나오지 않으면 바로 six_count를 초기화해줌
+            else // 연속하는 6을 찾기 위해 자릿수에서 6이 나오지 않으면 바로 six_count를 0으로 초기화해줌
                 six_count = 0;
 
             if (six_count == 3) // 연속하는 6이 3개 나온 경우

9e5083750376823334285bd95ca7442f1792f2bc
juhyeongkim527 <kesnt8892@gmail.com>
2024-07-10 14:47:33 +0900

    solved

---
+++ 4949/4949.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Stack
+{
+private:
+    int size;
+    vector<int> stack; // 크기가 정해지지 않았기 때문에 vector로 구현
+
+public:
+    Stack()
+    {
+        this->size = 0;
+    }
+
+    void insert(int val)
+    {
+        this->stack.push_back(val);
+        this->size++;
+    }
+
+    void pop()
+    {
+        this->size--; // main에서 is_empty가 아닐때만 pop을 호출하므로 is_empty 조건은 여기서 확인 안했음
+        stack.pop_back();
+    }
+
+    int is_empty()
+    {
+        return this->size == 0;
+    }
+
+    int front()
+    {
+        return this->stack[this->size - 1]; // main에서 is_empty가 아닐때만 front를 호출하므로 is_empty 조건은 여기서 확인 안했음
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}

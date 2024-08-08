3a944e8d4c96df87fc858b781436de90b5449345
juhyeongkim527 <kesnt8892@gmail.com>
2024-08-09 07:26:54 +0900

    solved

--- 1629/1629.cpp
+++ 1629/1629.cpp
@@ -15,10 +15,6 @@ long recursion(int exponent)
     if (memoization[exponent] != 0)
         return memoization[exponent];
 
-    // dp를 쓰면 최대 범위가 2147483647이므로 메모리 초과남
-    // if (dp[exponent] != -1) // 나머지 연산이므로 0이 될 수 있기 때문
-    //     return dp[exponent];
-
     if (exponent % 2 == 0) // 지수가 짝수인 경우
         return memoization[exponent] = (recursion(exponent / 2) * recursion(exponent / 2)) % c;
     else // 지수가 홀수인 경우
